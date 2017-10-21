106a5fbd276f5f7d32d37169d56027083272ec56
Tyler Hackbarth <tyler@searium.net>
2017-10-21 13:39:03 -0500

    Added all phases and cleaned up code

--- watcher.py
+++ watcher.py
@@ -7,46 +7,90 @@ import threading
 import datetime
 import sys
 
-pyautogui.PAUSE = 1
+# Setup
+# Probably need to redo images per machine, but you can test
+# Check for screen coordinate translation
+# Might need to adjust screenshot region
+# Turn on continuous auto for everything
+# Create a filter for friend units so no companion will ever be displayed (1* with level 8 black magic)
+# Must start at earth shrine quest select
+
 pyautogui.FAILSAFE = True
 
-# load images
-print("Loading images...")
-earth_shrine_label_img = Image.open("esLabel.png")
-earth_shrine_ent_img = Image.open("esEntLabel.png")
-select_companion_img = Image.open("selectCompanion.png")
-depart_img = Image.open("depart.png")
-es_done_img = Image.open("esDone.png")
-items_obtained_img = Image.open("itemsObtained.png")
-add_companion_img = Image.open("addCompanion.png")
-print("Images loaded...")
+screenshotRegion = (0, 0, 1100, 2000)
+# ss = pyautogui.screenshot(region=screenshotRegion)
+# ss.save('screenshot.png')
 
-def comp_images(im1, im2):
-	return ImageChops.difference(im1, im2).getbbox() is None
+def translateScreenLocation(location):
+	return tuple([x/2 for x in location])
 
-def click(location):
-	pyautogui.mouseDown(location, button='left')
-	time.sleep(1.0)
-	pyautogui.mouseUp(location, button='left')
+def cleanLocateTuple(location):
+	x, y, *rest = location
+	return (x, y)
+
+def checkPhase(phaseImage):
+	screen = pyautogui.screenshot(region=screenshotRegion)
+	location = pyautogui.locate(phaseImage, screen, grayscale=True)
+	return location
+
+def clickButton(buttonImage):
+	screen = pyautogui.screenshot(region=screenshotRegion)
+	location = pyautogui.locate(buttonImage, screen, grayscale=True)
+	success = False
+	if location:
+		location = cleanLocateTuple(location)
+		location = translateScreenLocation(location)
+		pyautogui.click(location, button='left')
+		success = True
+	return success
+
+def processPhase(phaseImage, buttonImage):
+	location = checkPhase(phaseImage)
+	success = False
+	if location:
+		if buttonImage:
+			success = clickButton(buttonImage)
+		else:
+			location = cleanLocateTuple(location)
+			location = translateScreenLocation(location)
+			pyautogui.click(location, button='left')
+			success = True
+	return success
+
 
 def process():
 	print("Starting bot...")
+	curPhase = "esEntrance"
 	while True:
-		time.sleep(5.0)
-		location = pyautogui.locateCenterOnScreen("selectES.png")
-
-		if (location):
-			print("ES Label found")
-			location = tuple([x/2 for x in location])
-			click(location)
-
+		print(curPhase)
+		if curPhase == "esEntrance":
+			if processPhase("esEntrance.png", None):
+				curPhase = "esMissions"
+		elif curPhase == "esMissions":
+			if processPhase("esMissionPage.png", "nextBtn.png"):
+				curPhase = "companionSelect"
+		elif curPhase == "companionSelect":
+			if processPhase("companionSelect.png", "departWithoutCompanion.png"):
+				curPhase = "depart"
+		elif curPhase == "depart":
+			if processPhase("esMissionPage.png", "departBtn.png"):
+				curPhase = "results"
+		elif curPhase == "results":
+			if processPhase("results.png", "resultsNextBtn.png"):
+				curPhase = "resultsUnitExp"
+		elif curPhase == "resultsUnitExp":
+			if processPhase("resultsUnitExp.png", None):
+				curPhase = "resultsItemsObtained"
+		elif curPhase == "resultsItemsObtained":
+			if processPhase("resultsItemsObtained.png", "itemsObtainedNextBtn.png"):
+				curPhase = "esEntrance"
 
 thread = threading.Thread(target=process)
 thread.daemon = True
 thread.start()
 
 try :
-	input('Press enter to end the program')
+	input('Press enter to end\n')
 except SyntaxError:
 	sys.exit(0)
 sys.exit(0)
